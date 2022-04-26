9aa1054a0c82f319b663d1edd76bfc7fa79f7322
Wei Loong <weiloong@netizenexperience.com>
2022-04-26 10:58:19 +0800

    project setup

---
+++ styles/theme/theme.ts
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "hsl(0, 100%, 50%)",
+    },
+    otherColor: {
+      main: "#999",
+    },
+  },
+});
