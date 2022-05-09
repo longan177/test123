1e1e9d0d351f07a7753a4304d86e68c69056525e
Wei Loong <weiloong@netizenexperience.com>
2022-05-09 13:37:53 +0800

    removed unuse important and updated sx props in layout.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import type { NextPage } from "next";
 import ShipList from "../component/Battleships/ShipList";
 import BoardLayout from "../component/BoardLayout/BoardLayout";
 import Layout from "../component/Layout";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
